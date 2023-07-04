cc94a0a66e289ad8ae900a32f6fa9687dd5fbabb
vusalzeynalovvz <112488906+vusalzeynalovvz@users.noreply.github.com>
2023-07-04 18:35:31 +0400

    Add files via upload

---
+++ Tasks/ConsoleApp1/ConsoleApp1/Month.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace ConsoleApp1
+//{
+//    enum Month
+//    {
+//        January = 1,
+//        February,
+//        March,
+//        April,
+//        May,
+//        June,
+//        July,
+//        August,
+//        September,
+//        October,
+//        November,
+//        December
+//    }
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Console.Write("Enter a number from 1 to 12: ");
+//            int userInput = int.Parse(Console.ReadLine());
+
+//            Month month = (Month)userInput;
+//            Console.WriteLine("The month is: " + month);
+//        }
+//    }
+//}
+
+////2) User-dan 1den 12-e geder bir eded qebul edin ve inputa gore duzgun ayi konsolda gorsedin.
---
+++ Tasks/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿//using System;
+
+//namespace ConsoleApp1
+//{
+
+//    interface IPerson 
+//    {
+//        void Speak();
+    
+//    }
+
+//    public class RussianPerson : IPerson
+//    {
+//        public void Speak()
+//        {
+//            Console.WriteLine("URusca Salam");
+//        }
+//    }
+//    public class FrenchPerson : IPerson
+//    {
+//        public void Speak()
+//        {
+//            Console.WriteLine("Bonjour");
+//        }
+//    }
+//    public class SpanishPerson : IPerson
+//    {
+//        public void Speak()
+//        {
+//            Console.WriteLine("Hola");
+//        }
+//    }
+
+
+
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            RussianPerson russianPerson = new RussianPerson();
+//            FrenchPerson frenchPerson = new FrenchPerson();
+//            SpanishPerson spanishPerson = new SpanishPerson();
+//            russianPerson.Speak();
+//            frenchPerson.Speak();
+//            spanishPerson.Speak();
+            
+//        }
+//    }
+//}
+
+////3).Elave olaraq 3 dene class olmalidi : RussianPerson, FrenchPerson, SpanishPerson.Her uch class IPerson 
+////interface-ni implement elemelidi.Her uch klassda Speak() metodun icherisinde sadece hemin dilde "salam" sozunu console-da chixardmalisiniz.
